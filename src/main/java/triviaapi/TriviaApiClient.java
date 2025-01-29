e58c8c39c7195f777537a27f58874d02161f576c
dpolymenakou <dpolymenakou@gmail.com>
2025-01-29 17:51:36 +0200

    test

--- src/main/java/triviaapi/TriviaApiClient.java
+++ src/main/java/triviaapi/TriviaApiClient.java
@@ -1,33 +1,34 @@
 package triviaapi;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class TriviaApiClient {
-
-private static final String API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
+	private static final String API_URL = "https://opentdb.com/api.php?amount=10&type=multiple";
 	
-public Response getTriviaQuestions() throws Exception {
+	public Response getTriviaQuestions() throws Exception {
+		URL url = new URL(API_URL);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
 
-URL url = new URL(API_URL);
-HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-connection.setRequestMethod("GET");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		StringBuilder response = new StringBuilder();
+		String line;
+		while ((line=reader.readLine()) != null) {
+			response.append(line);
+		}
 
-BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-StringBuilder response = new StringBuilder();
-String line;
-
-while ((line=reader.readLine()) != null) {
-	response.append(line);
-}
-
-reader.close();
+		reader.close();
 
 //json-αντικειμενο//
-Gson gson = new Gson();
-return gson.fromJson(response.toString(), Response.class);
+		Gson gson = new Gson();
+		try{
+			return gson.fromJson(response.toString(), Response.class);
+		} 	catch (JsonSyntaxException e) {
 }
-} 
+}
+}
